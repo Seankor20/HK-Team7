5a81d7fc70f287beff1fc64c0f6e5919b0b27373
Pang Zi Yang <zypang04@gmail.com>
2025-08-23 20:09:06 +0800

    useContext for supabase client

---
+++ src/lib/supabase-context.tsx
@@ -0,0 +1,37 @@
+// lib/supabase-context.tsx
+'use client'
+
+import { createContext, useContext, useEffect, useState } from 'react'
+import { createBrowserClient } from '@supabase/ssr'
+import type { SupabaseClient } from '@supabase/supabase-js'
+
+type SupabaseContext = {
+  supabase: SupabaseClient
+}
+
+const Context = createContext<SupabaseContext | undefined>(undefined)
+
+export default function SupabaseProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [supabase] = useState(() => createBrowserClient(
+    import.meta.env.VITE_SUPABASE_URL!,
+    import.meta.env.VITE_SUPABASE_ANON_KEY!
+  ))
+
+  return (
+    <Context.Provider value={{ supabase }}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export const useSupabase = () => {
+  const context = useContext(Context)
+  if (context === undefined) {
+    throw new Error('useSupabase must be used inside SupabaseProvider')
+  }
+  return context.supabase
+}
